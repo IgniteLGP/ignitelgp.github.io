77facd27620b886f3148a5623292526a7a759513
ferrolho <henriqueferrolho@gmail.com>
2016-04-02 00:57:37 +0100

    added social links; removed maps js

--- js/custom.js
+++ js/custom.js
@@ -151,5 +151,5 @@ function initialize() {
 
 }
 
-google.maps.event.addDomListener(window, "load", initialize);
+//google.maps.event.addDomListener(window, "load", initialize);
 // ========== END GOOGLE MAP ========== //
