428a2e8283736c5eae8ef116488e6f9c6c3a4099
ferrolho <henriqueferrolho@gmail.com>
2016-04-01 17:48:46 +0100

    added weMote logo; added gradient to navbar and footer

--- js/custom.js
+++ js/custom.js
@@ -30,13 +30,17 @@ $(document).ready(function(){
 		}
 	});
 	
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 400) {
-            $("#navigation").css("background-color","rgb(232, 98, 37)");
-        } else {
-            $("#navigation").css("background-color","rgba(16, 22, 54, 0.2)");
-        }
-    });
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 400) {
+			$("#navigation").css("background","rgb(232, 98, 37)");
+			$("#navigation").css("background", "-moz-linear-gradient(left, #a81e23 0%, #e71e25 33%, #e86225 66%, #e7c806 100%)");
+			$("#navigation").css("background", "-webkit-linear-gradient(left, #a81e23 0%, #e71e25 33%, #e86225 66%, #e7c806 100%)");
+			$("#navigation").css("background", "linear-gradient(to right, #a81e23 0%, #e71e25 33%, #e86225 66%, #e7c806 100%)");
+			$("#navigation").css("filter", "progid:DXImageTransform.Microsoft.gradient(startColorstr='#a81e23', endColorstr='#e7c806', GradientType=1)");
+		} else {
+			$("#navigation").css("background","rgba(16, 22, 54, 0.2)");
+		}
+	});
 	
 	/* ========================================================================= */
 	/*	Fix Slider Height
@@ -59,21 +63,21 @@ $(document).ready(function(){
     // portfolio filtering
 
     $(".project-wrapper").mixItUp();
-	
-	
-	$(".fancybox").fancybox({
-		padding: 0,
 
-		openEffect : 'elastic',
-		openSpeed  : 650,
 
-		closeEffect : 'elastic',
-		closeSpeed  : 550,
+    $(".fancybox").fancybox({
+    	padding: 0,
 
-		closeClick : true,
-	});
-	
-	/* ========================================================================= */
+    	openEffect : 'elastic',
+    	openSpeed  : 650,
+
+    	closeEffect : 'elastic',
+    	closeSpeed  : 550,
+
+    	closeClick : true,
+    });
+
+    /* ========================================================================= */
 	/*	Parallax
 	/* ========================================================================= */	
 	
@@ -84,66 +88,66 @@ $(document).ready(function(){
 	/* ========================================================================= */
 
 	"use strict";
-    $(".number-counters").appear(function () {
-        $(".number-counters [data-to]").each(function () {
-            var e = $(this).attr("data-to");
-            $(this).delay(6e3).countTo({
-                from: 50,
-                to: e,
-                speed: 3e3,
-                refreshInterval: 50
-            })
-        })
-    });
+	$(".number-counters").appear(function () {
+		$(".number-counters [data-to]").each(function () {
+			var e = $(this).attr("data-to");
+			$(this).delay(6e3).countTo({
+				from: 50,
+				to: e,
+				speed: 3e3,
+				refreshInterval: 50
+			})
+		})
+	});
 	
 	/* ========================================================================= */
 	/*	Back to Top
 	/* ========================================================================= */
 	
 	
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 400) {
-            $("#back-top").fadeIn(200)
-        } else {
-            $("#back-top").fadeOut(200)
-        }
-    });
-    $("#back-top").click(function () {
-        $("html, body").stop().animate({
-            scrollTop: 0
-        }, 1500, "easeInOutExpo")
-    });
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 400) {
+			$("#back-top").fadeIn(200)
+		} else {
+			$("#back-top").fadeOut(200)
+		}
+	});
+	$("#back-top").click(function () {
+		$("html, body").stop().animate({
+			scrollTop: 0
+		}, 1500, "easeInOutExpo")
+	});
 	
 });
 
 
 // ==========  START GOOGLE MAP ========== //
 function initialize() {
-    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
+	var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
 
-    var mapOptions = {
-        zoom: 14,
-        center: myLatLng,
-        disableDefaultUI: true,
-        scrollwheel: false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        draggable: false,
-        mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
-        }
-    };
+	var mapOptions = {
+		zoom: 14,
+		center: myLatLng,
+		disableDefaultUI: true,
+		scrollwheel: false,
+		navigationControl: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		draggable: false,
+		mapTypeControlOptions: {
+			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
+		}
+	};
 
-    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+	var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: 'img/location-icon.png',
-        title: '',
-    });
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		icon: 'img/location-icon.png',
+		title: '',
+	});
 
 }
 
